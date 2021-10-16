3166342c94d330d88b5a3e2ec2034db1af228b7f
wojdob <wojciech.dobrychlop@gmail.com>
2021-10-16 18:11:57 +0200

    Refactor code

--- tarot.py
+++ tarot.py
@@ -1,15 +1,14 @@
 import utils
-import os
 
-	
+
 """Console application to understand your fate"""
 
 if __name__ == '__main__':
-	answer = input("Would you like to know your fate?\n")
+    answer = input("Would you like to know your fate?\n")
 
-	if answer.lower() in ["yes", "yes please", "of course", "why not?", "sure", "yea", "yeah", "yup", "yep", "y"]:
-		deck = utils.getDeck()
-		deck = utils.shuffling(deck)
-		utils.threeCardSpread(deck)
-		utils.saveDeck(deck)
-	exit()	
+    if answer.lower() in ["yes", "yes please", "of course", "why not?", "sure", "yea", "yeah", "yup", "yep", "y"]:
+        deck = utils.getDeck()
+        deck = utils.shuffleDeck(deck)
+        utils.threeCardSpread(deck)
+        utils.saveDeck(deck)
+    exit()
